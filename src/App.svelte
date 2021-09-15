7b193b04a9ee797aef98b4ec0072a94e709fd496
Jacob Stordahl <jacob@stordahl.dev>
2021-09-15 12:55:13 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,139 @@
+<script>
+</script>
+
+<main>
+	<div style="grid-area: base;">
+		<p class="label">Header Default Examples</p>
+		<h1>Header One</h1>
+			<hr>
+		<h2>Header Two</h2>
+			<hr>
+		<h3>Header Three</h3>
+			<hr>
+		<h4>Header Four</h4>
+			<hr>
+		<h5>Header Five</h5>
+			<hr>
+		<h6>Header Six</h6>
+			<hr>
+		<p class="label">Paragraph Example</p>
+		<p>Paragraph Lorem ipsum dolor, sit amet <a href="/">consectetur</a> adipisicing elit. Deserunt molestias eos impedit debitis at vero repellendus doloremque vel cupiditate eius. Quisquam hic beatae perspiciatis, facilis dolore sequi voluptatibus aperiam ratione?
+		Magnam adipisci dolor explicabo facere commodi, numquam, sapiente vitae ex provident, dolores facilis a ipsum repellat blanditiis optio ad est reprehenderit eum corporis praesentium voluptatibus libero obcaecati. Quae, quos alias.
+		Ipsam quasi tempora hic iusto natus.</p>
+
+		
+	</div>
+	<div style="grid-area: blocks;">
+		<p class="label">Card Example</p>
+		<article class="card">
+			<a href="/">
+				<img class="card__image" src="https://hennepintech.edu/_images/academic-programs/building-and-construction/carpentry/Carpentry_banner3.jpg" alt="">
+				<header class="card__header">
+					<h2 class="card__title">Card Title</h2>
+					<span class="card__pub-date">Sun. Sept 12th, 2021</span>
+					<p class="card__description">a description of the article...</p>
+				</header>
+			</a>	
+		</article>
+		<!-- <pre>
+			<code>
+				&lt;article class=&quot;card&quot;&gt;<br/>  &lt;a href=&quot;/&quot;&gt;<br/>    &lt;img class=&quot;card__image&quot; src=&quot; source URL &quot; alt=&quot;&quot;&gt;<br/>      &lt;header class=&quot;card__header&quot;&gt;<br/>        &lt;h2 class=&quot;card__title&quot;&gt;Card Title&lt;/h2&gt;<br/>        &lt;span class=&quot;card__pub-date&quot;&gt;Sun. Sept 12th, 2021&lt;/span&gt;<br/>        &lt;p class=&quot;card__description&quot;&gt;a description of the article...&lt;/p&gt;<br/>      &lt;/header&gt;<br/>  &lt;/a&gt;	<br/>&lt;/article&gt;
+			</code>
+		</pre> -->
+
+
+		<p class="label">Callout Example</p>
+		<article class="callout">
+			<a href="/">
+				<img class="callout__image" src="https://hennepintech.edu/_images/home/1-500x500-ONLINE.jpg" alt="">
+				<header class="callout__header">
+					<h2 class="callout__title">Learn More</h2>
+				</header>
+			</a>
+		</article>
+		<!-- <pre>
+			<code>
+				&lt;article class=&quot;callout&quot;&gt;<br/>  &lt;a href=&quot;/&quot;&gt;<br/>    &lt;img class=&quot;callout__image&quot; src=&quot;source-URL&quot; alt=&quot;&quot;&gt;<br/>    &lt;header class=&quot;callout__header&quot;&gt;<br/>      &lt;h2 class=&quot;callout__title&quot;&gt;Learn More&lt;/h2&gt;<br/>    &lt;/header&gt;<br/>  &lt;/a&gt;<br/>&lt;/article&gt;
+			</code>
+		</pre> -->
+	</div>
+
+	<div style="grid-area: article;">
+		<p class="label">Article/Post Example</p>
+		<article class="article">
+			<header class="article__header">
+				<h2 class="article__title">Article Title</h2>
+				<div class="article__meta">
+					<p class="article__author">published by John Doe</p>
+					<p class="article__pub-date">Sun. Sept 12th, 2021</p>
+				</div>
+			</header>
+			<section class="article__section">
+				<h3 class="article__section__title">Section Title</h3>
+				<p class="article__section__paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ad ullam expedita minima, distinctio odio saepe consequatur atque quasi totam ratione blanditiis! Aut architecto animi eius, consectetur culpa deserunt maiores?
+					Cum voluptatem non eum architecto exercitationem ratione doloremque ducimus sapiente est hic. Velit voluptate autem architecto optio praesentium minus corrupti? Officia culpa ipsum fuga doloribus! Quasi voluptates repellendus iure laboriosam!</p>
+			</section>
+			<section class="article__section--two-column">
+				<h3 class="article__section__title">Section With an Image</h3>
+				<div class="article__section__inner">
+					<p class="article__section__paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ad ullam expedita minima, distinctio odio saepe consequatur atque quasi totam ratione blanditiis! Aut architecto animi eius, consectetur culpa deserunt maiores?
+						Cum voluptatem non eum architecto exercitationem ratione doloremque ducimus sapiente est hic. Velit voluptate autem architecto optio praesentium minus corrupti? Officia culpa ipsum fuga doloribus! Quasi voluptates repellendus iure laboriosam!
+						Dolorum nemo reiciendis suscipit! Quae et ex distinctio molestias earum eligendi repellendus rem at laborum blanditiis odit, sint ullam magnam iste vero tenetur enim quibusdam reprehenderit recusandae omnis magni? Facilis!
+						Aperiam officia laudantium voluptate, magnam pariatur provident iure, consequatur sit nemo quo quia molestiae fugit obcaecati minus aspernatur quis accusantium explicabo possimus rem enim tempore autem consectetur aliquid. Rerum, minus.</p>
+					<figure class="article__section__figure">
+						<img class="article__section__image" src="https://hennepintech.edu/communications/news/2021/Merrill-Irving-Headshot.jpg" alt=""/>
+						<figcaption class="article__section__figcaption">A portrait of HTC President Dr. Merrill Erving</figcaption>
+					</figure>	
+				</div>
+			</section>
+			<section class="article__section">
+				<h3 class="article__section__title">Another Section Title</h3>
+				<p class="article__section__paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ad ullam expedita minima, distinctio odio saepe consequatur atque quasi totam ratione blanditiis! Aut architecto animi eius, consectetur culpa deserunt maiores?
+				Cum voluptatem non eum architecto exercitationem ratione doloremque ducimus sapiente est hic. Velit voluptate autem architecto optio praesentium minus corrupti? Officia culpa ipsum fuga doloribus! Quasi voluptates repellendus iure laboriosam!</p>
+			</section>
+			
+		</article>
+		<!-- <pre>
+			<code>
+				&lt;article class=&quot;article&quot;&gt;<br/>  &lt;header class=&quot;article__header&quot;&gt;<br/>    &lt;h2 class=&quot;article__title&quot;&gt;Article Title&lt;/h2&gt;<br/>    &lt;div class=&quot;article__meta&quot;&gt;<br/>      &lt;p class=&quot;article__author&quot;&gt;published by John Doe&lt;/p&gt;<br/>      &lt;p class=&quot;article__pub-date&quot;&gt;Sun. Sept 12th, 2021&lt;/p&gt;<br/>    &lt;/div&gt;<br/>  &lt;/header&gt;<br/>  &lt;section class=&quot;article__section&quot;&gt;<br/>    &lt;h3 class=&quot;article__section__title&quot;&gt;Section Title&lt;/h3&gt;<br/>    &lt;p class=&quot;article__section__paragraph&quot;&gt;Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut ad ullam expedita minima, distinctio odio saepe consequatur atque quasi totam ratione blanditiis! Aut architecto animi eius, consectetur culpa deserunt maiores? &lt;/p&gt;<br/>  &lt;/section&gt;<br/>&lt;/article&gt;
+			</code>
+		</pre> -->
+	</div>
+
+</main>
+
+<style>
+	.label {
+		font-size: 2rem;
+		background-color: rgb(247, 247, 118);
+		padding: .2rem;
+		width: 100%;
+		margin-top: 1.5rem;
+	}
+
+	pre {
+		margin: 2rem auto;
+		max-width: 500px;
+		overflow: scroll;
+	}
+
+	main {
+		padding: 1em;
+		max-width: 90vw;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: 80vw;
+			display: grid;
+			grid-template-columns: calc(43vw - 3rem) calc(43vw - 3rem);
+			grid-template-rows: auto;
+			grid-template-areas:
+				"base blocks"
+				"article article"
+			;
+			gap: 3rem;
+		}
+	}
+</style>
